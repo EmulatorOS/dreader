911f46742c831cffb097acc81755b7a76165150d
EmulatorOS <102191619+EmulatorOS@users.noreply.github.com>
2022-04-28 21:55:28 -0500

    Create gameclient.class.js

--- js/core/gameclient.class.js
+++ js/core/gameclient.class.js
@@ -1,5 +1,5 @@
 /**
- * @file entity.class.js
+ * @file gameclient.class.js
  * @version 1.1.0
  * @license MIT License
  * Copyright 2020 Donitz
@@ -20,72 +20,116 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 
-import Options from '../core/options.class.js';
+import AudioManager from './audiomanager.class.js';
+import Context from './context.class.js';
+import Input from './input.class.js';
+import ModelManager from './modelmanager.class.js';
+import Time from './time.class.js';
 
-export default class Entity {
+import MazeManager from './game/mazemanager.class.js';
+
+import config from '../resources/config.js';
+
+import MainScene from '../scenes/mainscene.class.js';
+
+import * as THREE from '../lib/three.js/three.module.js';
+
+export default class GameClient {
     static _init() {
-        if (this !== Entity) {
-            throw new Error('Static _init called from wrong class');
+        GameClient._instance = null;
+
+        window.addEventListener('load', () => {
+            let creditsVisible = false;
+
+            const elementLoading = document.querySelector('.loading');
+            const elementCredits = document.querySelector('.credits');
+            const linkShowCredits = document.querySelector('.show-credits');
+            linkShowCredits.addEventListener('click', () => {
+                creditsVisible = !creditsVisible;
+
+                GameClient._instance._updating = !creditsVisible;
+
+                Input.setCursorStyle();
+
+                elementCredits.style.visibility = creditsVisible ? 'visible' : 'hidden';
+            });
+
+            const font = new FontFace('DefaultFont', 'url(./fonts/Recursive-SemiBold.ttf)');
+
+            font.load()
+                .then(font => {
+                    document.fonts.add(font);
+                    return AudioManager.asyncLoadSounds();
+                })
+                .then(() => ModelManager.asyncLoadModels())
+                .then(() => MazeManager.asyncLoadData())
+                .then(() => {
+                    elementLoading.style.visibility = 'hidden';
+                    linkShowCredits.style.visibility = 'visible';
+
+                    GameClient._instance = new GameClient();
+                    GameClient._instance.setScene(MainScene);
+                });
+        });
+    }
+
+    constructor() {
+        this._canvas = document.querySelector('canvas');
+
+        this._clock = new THREE.Clock(false);
+
+        this._renderer = new THREE.WebGLRenderer({
+            canvas: this._canvas,
+            antialias: config.renderer.antialias,
+        });
+        this._renderer.setPixelRatio(window.devicePixelRatio / config.renderer.pixelSize);
+        this._renderer.setClearColor('#001a00');
+
+        this._time = new Time();
+        this._context = new Context(this, this._time, this._renderer);
+
+        this._scene = null;
+
+        window.addEventListener('resize', this._handleResize.bind(this));
+
+        Input.setContextMenuEnabled(false);
+
+        this._updating = true;
+
+        this._clock.start();
+        this._renderer.setAnimationLoop(this._update.bind(this));
+    }
+
+    _handleResize() {
+        this._renderer.setSize(window.innerWidth, window.innerHeight);
+
+        if (this._scene !== null) {
+            this._scene.handleResize(this._context);
+        }
+    }
+
+    _update(time) {
+        this._time.addElapsedSeconds(Math.min(this._clock.getDelta(), config.maxTimestep));
+
+        if (this._updating) {
+            this._scene.update(this._context);
         }
 
-        Entity._typeByName = new Map();
+        Input.resetState();
     }
 
-    constructor(context, scene, options) {
-        options = Options.castIfRequired(options);
-
-        this._scene = scene;
-
-        this._destroyed = false;
-
-        this._updatePriority = options.get('updatePriority', 0);
-
-        scene.addEntity(this);
-    }
-
-    getScene() {
-        return this._scene;
-    }
-
-    getDestroyed() {
-        return this._destroyed;
-    }
-
-    destroy(context) {
-        if (this._destroyed) {
-            throw new Error('Entity has already been destroyed');
+    setScene(type) {
+        if (this._scene !== null) {
+            this._scene.destroy(this._context);
         }
-
-        this._destroyed = true;
-
-        this._scene.removeEntity(this);
+        this._scene = new type(this._context);
+        this._handleResize();
     }
 
-    handleResize(context) {
-    }
-
-    update(context) {
-    }
-
-    static p_register() {
-        Entity._typeByName.set(this.name, this);
-    }
-
-    static createByName(name, context, scene, options) {
-        return new (Entity.getTypeByName(name))(context, scene, options);
-    }
-
-    static getTypeByName(name) {
-        const type = Entity._typeByName.get(name);
-        if (type === undefined) {
-            throw new Error(`Entity type "${name}" has not been registered`);
-        }
-        return type;
-    }
-
-    static updatePrioritySortFunc(a, b) {
-        return b._updatePriority - a._updatePriority;
+    hideTitle() {
+        document.querySelector('.credits').style.visibility = 'hidden';
+        document.querySelector('.show-credits').style.visibility = 'hidden';
     }
 }
 
-Entity._init();
+GameClient._init();
